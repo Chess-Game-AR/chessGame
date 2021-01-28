143a520666f31c682aa75e9dd9c9d9c915d03f32
rohanvan123 <67704979+rohanvan123@users.noreply.github.com>
2021-01-27 23:53:10 -0600

    Added Spot class
    added spot class and variables in Board

--- Board.java
+++ Board.java
@@ -1,8 +1,9 @@
 public class Board {
     private final int WIDTH = 8;
     private final int HEIGTH = 8;
+    private int[][] Board;
     public Board() {
-
+        
     }
 
 }
---
+++ Spot.java
@@ -0,0 +1,8 @@
+public class Spot {
+    private int x;
+    private int y;
+    public Spot(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
