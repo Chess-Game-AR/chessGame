8088f433cb38f68bbb58b802ed13f9d1aedfb91e
rohanvan123 <67704979+rohanvan123@users.noreply.github.com>
2021-01-28 15:44:22 -0600

    Updated Piece
    updated the piece class

--- Piece.java
+++ Piece.java
@@ -1,3 +1,21 @@
 public abstract class Piece {
-    
+    private boolean white = false ; 
+    private boolean killed = false;
+
+    public Piece(boolean white) {
+        this.setWhite(white);
+    }
+
+    public boolean isWhite() {
+        return this.white;
+    }
+    public void setWhite(boolean white) {
+        this.white = white; 
+    }
+    public boolean isKilled() {
+        return this.killed;
+    }
+    public void setKilled(boolean killed) {
+        this.killed = killed; 
+    }
 }
--- Spot.java
+++ Spot.java
@@ -1,16 +1,19 @@
 public class Spot {
     private int x;
     private int y;
+    //constructor
     public Spot(int x, int y) {
         this.x = x;
         this.y = y;
     }
+    //accessors
     public int getX() {
         return this.x;
     }
     public int getY() {
         return this.y;
     }
+    //mutators
     public void setX(int x) {
         this.x = x;
     }
