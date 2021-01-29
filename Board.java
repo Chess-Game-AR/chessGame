9f7cf6ac09530e573d39ed4e8a352c8dd9fd1ce7
rohanvan123 <67704979+rohanvan123@users.noreply.github.com>
2021-01-29 10:11:15 -0600

    troubleshooted

--- Board.java
+++ Board.java
@@ -3,15 +3,15 @@ public class Board {
     private final int HEIGHT = 8;
     private Spot[][] Board;
     public Board() {
+        Piece rook = new Rook(false);
         this.Board = new Spot[WIDTH][HEIGHT];
         for(int i = 0; i < WIDTH; i++) {
             for(int j = 0; j < HEIGHT; j++) {
-                Board[i][j] = new Spot(i,j);
+                Board[i][j] = new Spot(i,j, rook);
             }
         }
     }
     public String getBoardSpot(int i, int j) {
-        return this.Board[i][j].getX() + ", " + this.Board[i][j].getY();
+        return "there is a " + this.Board[i][j].getPiece() + " at " + this.Board[i][j].getX() + ", " + this.Board[i][j].getY();
     }
-
 }
--- Spot.java
+++ Spot.java
@@ -1,10 +1,12 @@
 public class Spot {
     private int x;
     private int y;
+    private Piece p;
     //constructor
-    public Spot(int x, int y) {
+    public Spot(int x, int y, Piece p) {
         this.x = x;
         this.y = y;
+        this.p = p;
     }
     //accessors
     public int getX() {
@@ -13,11 +15,17 @@ public class Spot {
     public int getY() {
         return this.y;
     }
+    public Piece getPiece() {
+        return this.p;
+    }
     //mutators
     public void setX(int x) {
         this.x = x;
     }
     public void setY(int y) {
         this.y = y;
+    }
+    public void setPiece(Piece p) {
+        this.p = p;
     }
 }
